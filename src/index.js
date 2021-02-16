6e973fd64d45085fcb8cc0b0da9e3087e75340db
cobra21 <artur.zayczev18@gmail.com>
2021-02-16 09:57:02 +0500

    init

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import * as $ from 'jquery';
 import style from './sass/main.sass';
 import pug from './pages/index.pug';
 
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
 
 
